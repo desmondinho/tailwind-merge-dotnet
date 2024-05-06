9d96e9cfba38ab91ccfd7830de77022cd27b4b79
desmondinho <daniil.norinn@gmail.com>
2024-05-06 23:53:03 +0300

    Tests: Add colors classes tests

---
+++ tests/ColorsClassesTests.cs
@@ -0,0 +1,17 @@
+namespace TailwindMerge.Tests;
+
+public class ColorsClassesTests
+{
+    [Theory]
+    [InlineData( "bg-grey-5 bg-hotpink", "bg-hotpink" )]
+    [InlineData( "hover:bg-grey-5 hover:bg-hotpink", "hover:bg-hotpink" )]
+    [InlineData( "stroke-[hsl(350_80%_0%)] stroke-[10px]", "stroke-[hsl(350_80%_0%)] stroke-[10px]" )]
+    public void MergesColorClassesCorrectly( string classLists, string expected )
+    {
+        // Act
+        var actual = new TwMerge().Merge( classLists );
+
+        // Assert
+        Assert.Equal( expected, actual );
+    }
+}
