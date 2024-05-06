ba7624d5251d420c971a767bce7e204fb5253d90
desmondinho <daniil.norinn@gmail.com>
2024-05-06 20:15:30 +0300

    Add the service collection extension

---
+++ src/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TailwindMerge.Extensions;
+
+/// <summary>
+/// Provides extension methods to configure TailwindMerge services on a <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+	/// Registers a singleton instance of the <see cref="TwMerge"/> service.
+	/// </summary>
+	/// <param name="services">The <see cref="IServiceCollection"/>.</param>
+	public static void AddTailwindMerge( this IServiceCollection services )
+    {
+        services.AddSingleton<TwMerge>();
+    }
+}
