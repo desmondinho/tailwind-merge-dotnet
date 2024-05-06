b659c8f28d79bd16f19a3ac7c6a3eb2ac1624d0e
desmondinho <daniil.norinn@gmail.com>
2024-05-06 23:53:14 +0300

    Tests: Update standalone classes tests

--- tests/StandaloneClassesTests.cs
+++ tests/StandaloneClassesTests.cs
@@ -6,6 +6,12 @@ public class StandaloneClassesTests
     [InlineData( "inline block", "block" )]
     [InlineData( "underline line-through", "line-through" )]
     [InlineData( "line-through no-underline", "no-underline" )]
+    [InlineData( "hover:block hover:inline", "hover:inline" )]
+    [InlineData( "hover:inline hover:block", "hover:block" )]
+    [InlineData( 
+        "inline hover:inline focus:inline hover:block hover:focus:block", 
+        "inline focus:inline hover:block hover:focus:block" 
+    )]
     public void MergesStandaloneClassesFromSameGroupCorrectly( string classLists, string expected )
     {
         // Act
