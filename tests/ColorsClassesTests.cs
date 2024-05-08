56cda623df940eb0d64dbb071c6e0d3886ab7486
desmondinho <daniil.norinn@gmail.com>
2024-05-08 22:25:22 +0300

    Tests: nits

--- tests/ColorsClassesTests.cs
+++ tests/ColorsClassesTests.cs
@@ -5,7 +5,10 @@ public class ColorsClassesTests
     [Theory]
     [InlineData( "bg-grey-5 bg-hotpink", "bg-hotpink" )]
     [InlineData( "hover:bg-grey-5 hover:bg-hotpink", "hover:bg-hotpink" )]
-    [InlineData( "stroke-[hsl(350_80%_0%)] stroke-[10px]", "stroke-[hsl(350_80%_0%)] stroke-[10px]" )]
+    [InlineData( 
+        "stroke-[hsl(350_80%_0%)] stroke-[10px]", 
+        "stroke-[hsl(350_80%_0%)] stroke-[10px]" 
+    )]
     public void Merge_Colors_MergesCorrectly( string classLists, string expected )
     {
         // Act
