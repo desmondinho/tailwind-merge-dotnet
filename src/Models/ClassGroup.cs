c2bb9ed0d5d80879bbb0ceb63564ef66f884ecf2
desmondinho <daniil.norinn@gmail.com>
2024-06-24 22:42:39 -0500

    feat: `ClassGroup` accepts an array of objects as group definitions

--- src/Models/ClassGroup.cs
+++ src/Models/ClassGroup.cs
@@ -1,44 +1,34 @@
 ﻿namespace TailwindMerge.Models;
 
 /// <summary>
-/// Represents a group of classes with associated metadata for the <see cref="TwMergeConfig"/>.
+/// Represents a group of CSS classes with associated metadata for the TailwindMerge utility.
 /// </summary>
-/// <param name="Id">The unique identifier for the class group.</param>
-/// <param name="BaseClassName">The base class name of the class group.</param>
-/// <param name="ClassNameParts">An array of class name parts belonging to the group.</param>
-/// <param name="Validators">An array of validation functions for the class names.</param>
-public readonly record struct ClassGroup(
-    string Id,
-    string? BaseClassName,
-    string[]? ClassNameParts,
-    Func<string, bool>[]? Validators )
+/// <param name="id">The unique identifier for the class group.</param>
+/// <param name="baseClassName">The base class name for the group.</param>
+/// <param name="definitions">An array of definitions associated with the class group.</param>
+public readonly struct ClassGroup( string id, string? baseClassName, object[] definitions )
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="ClassGroup"/> struct 
-    /// with the specified <paramref name="id"/> and <paramref name="classNameParts"/>.
+    /// Gets the unique identifier for the class group.
     /// </summary>
-    /// <param name="id">The unique identifier for the class group.</param>
-    /// <param name="classNameParts">An array of class name parts belonging to the group.</param>
-    public ClassGroup( string id, string[] classNameParts )
-        : this( id, null, classNameParts, null ) { }
+    public string Id { get; } = id;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ClassGroup"/> struct 
-    /// with the specified <paramref name="id"/>, <paramref name="baseClassName"/>, and <paramref name="classNameParts"/>.
+    /// Gets the base class name for the group.
     /// </summary>
-    /// <param name="id">The unique identifier for the class group.</param>
-    /// <param name="baseClassName">The name of the class group.</param>
-    /// <param name="classNameParts">An array of class name parts belonging to the group.</param>
-    public ClassGroup( string id, string baseClassName, string[] classNameParts )
-        : this( id, baseClassName, classNameParts, null ) { }
+    public string? BaseClassName { get; } = baseClassName;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ClassGroup"/> struct 
-    /// with the specified <paramref name="id"/>, <paramref name="baseClassName"/>, and <paramref name="validators"/>.
+    /// Gets an array of definitions associated with the class group.
+    /// </summary>
+    public object[] Definitions { get; } = definitions;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClassGroup"/> struct with the 
+    /// specified <paramref name="id"/> and <paramref name="definitions"/>.
     /// </summary>
     /// <param name="id">The unique identifier for the class group.</param>
-    /// <param name="baseClassName">The name of the class group.</param>
-    /// <param name="validators">An array of validation functions for the class names.</param>
-    public ClassGroup( string id, string baseClassName, Func<string, bool>[] validators )
-        : this( id, baseClassName, null, validators ) { }
+    /// <param name="definitions">An array of definitions associated with the class group.</param>
+    public ClassGroup( string id, object[] definitions )
+        : this( id, null, definitions ) { }
 }
