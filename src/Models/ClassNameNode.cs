771f3102f57b3e31086f0fa7cede826f2d5984c4
desmondinho <daniil.norinn@gmail.com>
2024-06-30 21:06:31 -0500

    refactor: remove redundant field for validators in ClassNameNode

--- src/Models/ClassNameNode.cs
+++ src/Models/ClassNameNode.cs
@@ -4,11 +4,9 @@ namespace TailwindMerge.Models;
 
 internal record ClassNameNode
 {
-    private List<ClassValidator>? _validators;
-
     internal string? ClassGroupId { get; set; }
     internal Dictionary<string, ClassNameNode>? Next { get; set; }
-    internal IReadOnlyCollection<ClassValidator>? Validators => _validators?.AsReadOnly();
+    internal List<ClassValidator>? Validators { get; set; }
 
     internal ClassNameNode AddNextNode( string className )
     {
@@ -33,7 +31,7 @@ internal record ClassNameNode
 
     internal void AddValidator( Func<string, bool> validator, string classGroupId )
     {
-        _validators ??= [];
-        _validators.Add( new ClassValidator( classGroupId, validator ) );
+        Validators ??= [];
+        Validators.Add( new ClassValidator( classGroupId, validator ) );
     }
 }
