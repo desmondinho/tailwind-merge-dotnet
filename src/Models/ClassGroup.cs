002f65307ae8ddf97eb3396be60431bd83acb5ba
desmondinho <daniil.norinn@gmail.com>
2024-06-30 21:05:35 -0500

    feat: simplify class groups

--- src/Models/ClassGroup.cs
+++ src/Models/ClassGroup.cs
@@ -3,16 +3,10 @@
 /// <summary>
 /// Represents a group of CSS classes with associated metadata for the TailwindMerge utility.
 /// </summary>
-/// <param name="id">The unique identifier for the class group.</param>
 /// <param name="baseClassName">The base class name for the group.</param>
 /// <param name="definitions">An array of definitions associated with the class group.</param>
-public readonly struct ClassGroup( string id, string? baseClassName, object[] definitions )
+public readonly struct ClassGroup( string? baseClassName, object[] definitions )
 {
-    /// <summary>
-    /// Gets the unique identifier for the class group.
-    /// </summary>
-    public string Id { get; } = id;
-
     /// <summary>
     /// Gets the base class name for the group.
     /// </summary>
@@ -29,6 +23,6 @@ public readonly struct ClassGroup( string id, string? baseClassName, object[] de
     /// </summary>
     /// <param name="id">The unique identifier for the class group.</param>
     /// <param name="definitions">An array of definitions associated with the class group.</param>
-    public ClassGroup( string id, object[] definitions )
-        : this( id, null, definitions ) { }
+    public ClassGroup( object[] definitions )
+        : this( null, definitions ) { }
 }
